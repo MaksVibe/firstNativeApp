297aef4e8d28d5caf64416cd6c3dcf6b4e06f9ce
Maksym <90103151+MaksVibe@users.noreply.github.com>
2022-07-06 09:43:25 +0300

    Start react native project

--- App.js
+++ App.js
@@ -1,10 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+} from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as Font from "expo-font";
 
 export default function App() {
-  return (
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const nameHandler = text => setName(text);
+  const passwordHandler = text => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+          "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return !appIsReady ? (
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>App is loading...</Text>
+      <Entypo name="rocket" size={30} />
+    </View>
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <TextInput
+          value={name}
+          onChangeText={nameHandler}
+          placeholder="Username"
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={passwordHandler}
+          placeholder="Password"
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <Button title={"Login"} style={styles.input} onPress={onLogin} />
+      </KeyboardAvoidingView>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +98,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: "Roboto-Regular",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...Platform.select({
+      ios: {
+        backgroundColor: "#cccccc",
+      },
+      android: {
+        backgroundColor: "#ffffff",
+      },
+    }),
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: Platform.OS === "ios" ? 50 : 30,
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
   },
 });
